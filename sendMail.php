597437d854094fd796d3809c42513a62a9566d1f
root <root@localhost.localdomain>
2019-07-26 07:28:42 -0500

    edit sendMail

--- sendMail.php
+++ sendMail.php
@@ -3,15 +3,15 @@
 if(isset($_POST['submit'])){
   $to = "dzsq1337@yandex.ru";
   $from = $_POST['email'];
-  $first_name = $_POST['name'];
-  $subject = "Соосщение с сайта";
+  $name = $_POST['name'];
+  $subject = "Сообщение с сайта";
   $message = $name . " оставил сообщение:" . "\n\n" . $_POST['message'];
 
   $headers = "From:" . $from;
 
   mail($to,$subject,$message,$headers);
   // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-  echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+  echo "Сообщение отправлено. Спасибо Вам " . $name . ", мы скоро свяжемся с Вами.";
   echo "<br /><br /><a href='https://epicblog.net'>Вернуться на сайт.</a>";
 }
 
